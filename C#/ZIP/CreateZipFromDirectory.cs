2eeae08eadd2f09dddb9c458b7ed8004df0b8ee6
i-vt <99633800+i-vt@users.noreply.github.com>
2024-01-19 01:15:35 -0500

    Update and rename C#/CreateZipFromDirectory.cs to C#/ZIP/CreateZipFromDirectory.cs

--- C#/ZIP/CreateZipFromDirectory.cs
+++ C#/ZIP/CreateZipFromDirectory.cs
@@ -3,16 +3,10 @@ using System.IO.Compression;
 
 public static void CreateZipFromDirectory(string strFolderToZip, string strDestinationDirectory)
 {
-    // Ensure the folder exists
     if (Directory.Exists(strFolderToZip))
     {
-        // Get the folder name from the folder path
         var folderName = new DirectoryInfo(strFolderToZip).Name;
-
-        // Construct the full path for the zip file
         string zipFilePath = Path.Combine(strDestinationDirectory, strFolderToZip + ".zip");
-
-        // Create a zip from the directory
         ZipFile.CreateFromDirectory(strFolderToZip, zipFilePath);
     }
     else
