8848f52763165557da73f2fecc70d5cca671159b
i-vt <99633800+i-vt@users.noreply.github.com>
2024-01-19 01:15:01 -0500

    Update and rename C#/CreateZipFile.cs to C#/ZIP/CreateZipFile.cs

--- C#/ZIP/CreateZipFile.cs
+++ C#/ZIP/CreateZipFile.cs
@@ -3,10 +3,8 @@ using System.IO.Compression;
 
 public static void CreateZipFile(string fileToZip, string destinationZipFullPath)
 {
-    // Ensure the file exists
     if (File.Exists(fileToZip))
     {
-        // Create a zip that contains the file
         using (var zipArchive = ZipFile.Open(destinationZipFullPath, ZipArchiveMode.Create))
         {
             zipArchive.CreateEntryFromFile(fileToZip, Path.GetFileName(fileToZip));
