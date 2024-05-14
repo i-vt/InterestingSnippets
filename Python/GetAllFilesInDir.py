05e2f0b47aa5c178ea36ee20521de9a2c58122f9
i-vt <99633800+i-vt@users.noreply.github.com>
2024-05-13 21:35:37 -0400

    Create GetAllFilesInDir.py

---
+++ Python/GetAllFilesInDir.py
@@ -0,0 +1,7 @@
+def get_all_dirs(start_dir: str = "/"):
+    pages = []
+    for root, dirs, files in os.walk(start_dir):
+        for filename in files:
+            page_path = os.path.join(root, filename)
+            pages.append(page_path)
+    return pages
