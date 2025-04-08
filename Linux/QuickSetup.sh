edf0b144ff6d7c45cb880cf4ebe1d58ec57dea16
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-08 09:33:36 -0400

    Update QuickSetup.sh

--- Linux/QuickSetup.sh
+++ Linux/QuickSetup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Update and upgrade the system packages
-sudo apt update && sudo apt upgrade || { echo 'Failed to update and upgrade packages'; exit 1; }
+# Update and upgrade the system packages non-interactively
+sudo apt update -y && sudo apt upgrade -y || { echo 'Failed to update and upgrade packages'; exit 1; }
 
 # Install necessary packages
-sudo apt install curl tree python3-pip plocate snapd python3-venv -y || { echo 'Failed to install packages'; exit 1; }
+sudo apt install -y curl tree python3-pip plocate snapd python3-venv || { echo 'Failed to install packages'; exit 1; }
 
 # Download and save the .vimrc file to the home directory
 wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=$HOME/.vimrc || { echo 'Failed to download .vimrc'; exit 1; }
