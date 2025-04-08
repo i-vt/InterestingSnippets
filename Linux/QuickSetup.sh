f4caf3d4338e3a0954d261e9751f189132705a7d
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-08 09:19:20 -0400

    Update QuickSetup.sh

--- Linux/QuickSetup.sh
+++ Linux/QuickSetup.sh
@@ -6,6 +6,9 @@ sudo apt upgrade
 sudo apt install curl tree python3-pip plocate snapd
 sudo apt install python3-venv
 
+wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=~/.vimrc
+
+
 echo "-------[current ip]-------"
 echo ""
 curl https://api.ipify.org/
