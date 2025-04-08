90ef5bb19f606865720ea1e4161d5be645344d05
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-08 09:30:52 -0400

    Update QuickSetup.sh

--- Linux/QuickSetup.sh
+++ Linux/QuickSetup.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-sudo apt update
-sudo apt upgrade
+# Update and upgrade the system packages
+sudo apt update && sudo apt upgrade || { echo 'Failed to update and upgrade packages'; exit 1; }
 
-sudo apt install curl tree python3-pip plocate snapd
-sudo apt install python3-venv
+# Install necessary packages
+sudo apt install -y curl tree python3-pip plocate snapd python3-venv || { echo 'Failed to install packages'; exit 1; }
 
-wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=~/.vimrc
+# Download and save the .vimrc file to the home directory
+wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=$HOME/.vimrc || { echo 'Failed to download .vimrc'; exit 1; }
 
-
+# Display the current IP address
 echo "-------[current ip]-------"
-echo ""
-curl https://api.ipify.org/
+curl https://api.ipify.org/ || { echo 'Failed to fetch IP address'; exit 1; }
 echo ""
