298398c659844a7a7ca3ce40dce155cb0724c1cf
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-10 01:36:06 -0400

    Create CountriesFlagAndPrefix.js

---
+++ Web/JS/CountriesFlagAndPrefix.js
@@ -0,0 +1,971 @@
+
+const countries = [
+{
+  name: "Afghanistan",
+  prefix: 93,
+  flag: "af" },
+
+{
+  name: "Albania",
+  prefix: 355,
+  flag: "al" },
+
+{
+  name: "Algeria",
+  prefix: 213,
+  flag: "dz" },
+
+{
+  name: "Andorra",
+  prefix: 376,
+  flag: "ad" },
+
+{
+  name: "Angola",
+  prefix: 244,
+  flag: "ao" },
+
+{
+  name: "Antigua and Barbuda",
+  prefix: 1,
+  flag: "ag" },
+
+{
+  name: "Argentina",
+  prefix: 54,
+  flag: "ar" },
+
+{
+  name: "Armenia",
+  prefix: 374,
+  flag: "am" },
+
+{
+  name: "Australia",
+  prefix: 61,
+  flag: "au" },
+
+{
+  name: "Austria",
+  prefix: 43,
+  flag: "at" },
+
+{
+  name: "Azerbaijan",
+  prefix: 994,
+  flag: "az" },
+
+{
+  name: "Bahamas",
+  prefix: 1,
+  flag: "bs" },
+
+{
+  name: "Bahrain",
+  prefix: 973,
+  flag: "bh" },
+
+{
+  name: "Bangladesh",
+  prefix: 880,
+  flag: "bd" },
+
+{
+  name: "Barbados",
+  prefix: 1,
+  flag: "bb" },
+
+{
+  name: "Belarus",
+  prefix: 375,
+  flag: "by" },
+
+{
+  name: "Belgium",
+  prefix: 32,
+  flag: "be" },
+
+{
+  name: "Belize",
+  prefix: 501,
+  flag: "bz" },
+
+{
+  name: "Benin",
+  prefix: 229,
+  flag: "bj" },
+
+{
+  name: "Bhutan",
+  prefix: 975,
+  flag: "bt" },
+
+{
+  name: "Bolivia",
+  prefix: 591,
+  flag: "bo" },
+
+{
+  name: "Bosnia and Herzegovina",
+  prefix: 387,
+  flag: "ba" },
+
+{
+  name: "Botswana",
+  prefix: 267,
+  flag: "bw" },
+
+{
+  name: "Brazil",
+  prefix: 55,
+  flag: "br" },
+
+{
+  name: "Brunei",
+  prefix: 673,
+  flag: "bn" },
+
+{
+  name: "Bulgaria",
+  prefix: 359,
+  flag: "bg" },
+
+{
+  name: "Burkina Faso",
+  prefix: 226,
+  flag: "bf" },
+
+{
+  name: "Burundi",
+  prefix: 257,
+  flag: "bi" },
+
+{
+  name: "Cabo Verde",
+  prefix: 238,
+  flag: "cv" },
+
+{
+  name: "Cambodia",
+  prefix: 855,
+  flag: "kh" },
+
+{
+  name: "Cameroon",
+  prefix: 237,
+  flag: "cm" },
+
+{
+  name: "Canada",
+  prefix: 1,
+  flag: "ca" },
+
+{
+  name: "Central African Republic",
+  prefix: 236,
+  flag: "cf" },
+
+{
+  name: "Chad",
+  prefix: 235,
+  flag: "td" },
+
+{
+  name: "Chile",
+  prefix: 56,
+  flag: "cl" },
+
+{
+  name: "China",
+  prefix: 86,
+  flag: "cn" },
+
+{
+  name: "Colombia",
+  prefix: 57,
+  flag: "co" },
+
+{
+  name: "Comoros",
+  prefix: 269,
+  flag: "km" },
+
+{
+  name: "Congo",
+  prefix: 242,
+  flag: "cg" },
+
+{
+  name: "Costa Rica",
+  prefix: 506,
+  flag: "cr" },
+
+{
+  name: "Croatia",
+  prefix: 385,
+  flag: "hr" },
+
+{
+  name: "Cuba",
+  prefix: 53,
+  flag: "cu" },
+
+{
+  name: "Cyprus",
+  prefix: 357,
+  flag: "cy" },
+
+{
+  name: "Czech Republic",
+  prefix: 420,
+  flag: "cz" },
+
+{
+  name: "Denmark",
+  prefix: 45,
+  flag: "dk" },
+
+{
+  name: "Djibouti",
+  prefix: 253,
+  flag: "dj" },
+
+{
+  name: "Dominica",
+  prefix: 1,
+  flag: "dm" },
+
+{
+  name: "Dominican Republic",
+  prefix: 1,
+  flag: "do" },
+
+{
+  name: "Ecuador",
+  prefix: 593,
+  flag: "ec" },
+
+{
+  name: "Egypt",
+  prefix: 20,
+  flag: "eg" },
+
+{
+  name: "El Salvador",
+  prefix: 503,
+  flag: "sv" },
+
+{
+  name: "Equatorial Guinea",
+  prefix: 240,
+  flag: "gq" },
+
+{
+  name: "Eritrea",
+  prefix: 291,
+  flag: "er" },
+
+{
+  name: "Estonia",
+  prefix: 372,
+  flag: "ee" },
+
+{
+  name: "Eswatini",
+  prefix: 268,
+  flag: "sz" },
+
+{
+  name: "Ethiopia",
+  prefix: 251,
+  flag: "et" },
+
+{
+  name: "Fiji",
+  prefix: 679,
+  flag: "fj" },
+
+{
+  name: "Finland",
+  prefix: 358,
+  flag: "fi" },
+
+{
+  name: "France",
+  prefix: 33,
+  flag: "fr" },
+
+{
+  name: "Gabon",
+  prefix: 241,
+  flag: "ga" },
+
+{
+  name: "Gambia",
+  prefix: 220,
+  flag: "gm" },
+
+{
+  name: "Georgia",
+  prefix: 995,
+  flag: "ge" },
+
+{
+  name: "Germany",
+  prefix: 49,
+  flag: "de" },
+
+{
+  name: "Ghana",
+  prefix: 233,
+  flag: "gh" },
+
+{
+  name: "Greece",
+  prefix: 30,
+  flag: "gr" },
+
+{
+  name: "Grenada",
+  prefix: 1,
+  flag: "gd" },
+
+{
+  name: "Guatemala",
+  prefix: 502,
+  flag: "gt" },
+
+{
+  name: "Guinea",
+  prefix: 224,
+  flag: "gn" },
+
+{
+  name: "Guinea-Bissau",
+  prefix: 245,
+  flag: "gw" },
+
+{
+  name: "Guyana",
+  prefix: 592,
+  flag: "gy" },
+
+{
+  name: "Haiti",
+  prefix: 509,
+  flag: "ht" },
+
+{
+  name: "Honduras",
+  prefix: 504,
+  flag: "hn" },
+
+{
+  name: "Hungary",
+  prefix: 36,
+  flag: "hu" },
+
+{
+  name: "Iceland",
+  prefix: 354,
+  flag: "is" },
+
+{
+  name: "India",
+  prefix: 91,
+  flag: "in" },
+
+{
+  name: "Indonesia",
+  prefix: 62,
+  flag: "id" },
+
+{
+  name: "Iran",
+  prefix: 98,
+  flag: "ir" },
+
+{
+  name: "Iraq",
+  prefix: 964,
+  flag: "iq" },
+
+{
+  name: "Ireland",
+  prefix: 353,
+  flag: "ie" },
+
+{
+  name: "Israel",
+  prefix: 972,
+  flag: "il" },
+
+{
+  name: "Italy",
+  prefix: 39,
+  flag: "it" },
+
+{
+  name: "Jamaica",
+  prefix: 1,
+  flag: "jm" },
+
+{
+  name: "Japan",
+  prefix: 81,
+  flag: "jp" },
+
+{
+  name: "Jordan",
+  prefix: 962,
+  flag: "jo" },
+
+{
+  name: "Kazakhstan",
+  prefix: 7,
+  flag: "kz" },
+
+{
+  name: "Kenya",
+  prefix: 254,
+  flag: "ke" },
+
+{
+  name: "Kiribati",
+  prefix: 686,
+  flag: "ki" },
+
+{
+  name: "Kuwait",
+  prefix: 965,
+  flag: "kw" },
+
+{
+  name: "Kyrgyzstan",
+  prefix: 996,
+  flag: "kg" },
+
+{
+  name: "Laos",
+  prefix: 856,
+  flag: "la" },
+
+{
+  name: "Latvia",
+  prefix: 371,
+  flag: "lv" },
+
+{
+  name: "Lebanon",
+  prefix: 961,
+  flag: "lb" },
+
+{
+  name: "Lesotho",
+  prefix: 266,
+  flag: "ls" },
+
+{
+  name: "Liberia",
+  prefix: 231,
+  flag: "lr" },
+
+{
+  name: "Libya",
+  prefix: 218,
+  flag: "ly" },
+
+{
+  name: "Liechtenstein",
+  prefix: 423,
+  flag: "li" },
+
+{
+  name: "Lithuania",
+  prefix: 370,
+  flag: "lt" },
+
+{
+  name: "Luxembourg",
+  prefix: 352,
+  flag: "lu" },
+
+{
+  name: "Madagascar",
+  prefix: 261,
+  flag: "mg" },
+
+{
+  name: "Malawi",
+  prefix: 265,
+  flag: "mw" },
+
+{
+  name: "Malaysia",
+  prefix: 60,
+  flag: "my" },
+
+{
+  name: "Maldives",
+  prefix: 960,
+  flag: "mv" },
+
+{
+  name: "Mali",
+  prefix: 223,
+  flag: "ml" },
+
+{
+  name: "Malta",
+  prefix: 356,
+  flag: "mt" },
+
+{
+  name: "Marshall Islands",
+  prefix: 692,
+  flag: "mh" },
+
+{
+  name: "Mauritania",
+  prefix: 222,
+  flag: "mr" },
+
+{
+  name: "Mauritius",
+  prefix: 230,
+  flag: "mu" },
+
+{
+  name: "Mexico",
+  prefix: 52,
+  flag: "mx" },
+
+{
+  name: "Micronesia",
+  prefix: 691,
+  flag: "fm" },
+
+{
+  name: "Moldova",
+  prefix: 373,
+  flag: "md" },
+
+{
+  name: "Monaco",
+  prefix: 377,
+  flag: "mc" },
+
+{
+  name: "Mongolia",
+  prefix: 976,
+  flag: "mn" },
+
+{
+  name: "Montenegro",
+  prefix: 382,
+  flag: "me" },
+
+{
+  name: "Morocco",
+  prefix: 212,
+  flag: "ma" },
+
+{
+  name: "Mozambique",
+  prefix: 258,
+  flag: "mz" },
+
+{
+  name: "Myanmar",
+  prefix: 95,
+  flag: "mm" },
+
+{
+  name: "Namibia",
+  prefix: 264,
+  flag: "na" },
+
+{
+  name: "Nauru",
+  prefix: 674,
+  flag: "nr" },
+
+{
+  name: "Nepal",
+  prefix: 977,
+  flag: "np" },
+
+{
+  name: "Netherlands",
+  prefix: 31,
+  flag: "nl" },
+
+{
+  name: "New Zealand",
+  prefix: 64,
+  flag: "nz" },
+
+{
+  name: "Nicaragua",
+  prefix: 505,
+  flag: "ni" },
+
+{
+  name: "Niger",
+  prefix: 227,
+  flag: "ne" },
+
+{
+  name: "Nigeria",
+  prefix: 234,
+  flag: "ng" },
+
+{
+  name: "North Korea",
+  prefix: 850,
+  flag: "kp" },
+
+{
+  name: "North Macedonia",
+  prefix: 389,
+  flag: "mk" },
+
+{
+  name: "Norway",
+  prefix: 47,
+  flag: "no" },
+
+{
+  name: "Oman",
+  prefix: 968,
+  flag: "om" },
+
+{
+  name: "Pakistan",
+  prefix: 92,
+  flag: "pk" },
+
+{
+  name: "Palau",
+  prefix: 680,
+  flag: "pw" },
+
+{
+  name: "Palestine",
+  prefix: 970,
+  flag: "ps" },
+
+{
+  name: "Panama",
+  prefix: 507,
+  flag: "pa" },
+
+{
+  name: "Papua New Guinea",
+  prefix: 675,
+  flag: "pg" },
+
+{
+  name: "Paraguay",
+  prefix: 595,
+  flag: "py" },
+
+{
+  name: "Peru",
+  prefix: 51,
+  flag: "pe" },
+
+{
+  name: "Philippines",
+  prefix: 63,
+  flag: "ph" },
+
+{
+  name: "Poland",
+  prefix: 48,
+  flag: "pl" },
+
+{
+  name: "Portugal",
+  prefix: 351,
+  flag: "pt" },
+
+{
+  name: "Qatar",
+  prefix: 974,
+  flag: "qa" },
+
+{
+  name: "Romania",
+  prefix: 40,
+  flag: "ro" },
+
+{
+  name: "Russia",
+  prefix: 7,
+  flag: "ru" },
+
+{
+  name: "Rwanda",
+  prefix: 250,
+  flag: "rw" },
+
+{
+  name: "Saint Kitts and Nevis",
+  prefix: 1,
+  flag: "kn" },
+
+{
+  name: "Saint Lucia",
+  prefix: 1,
+  flag: "lc" },
+
+{
+  name: "Saint Vincent and the Grenadines",
+  prefix: 1,
+  flag: "vc" },
+
+{
+  name: "Samoa",
+  prefix: 685,
+  flag: "ws" },
+
+{
+  name: "San Marino",
+  prefix: 378,
+  flag: "sm" },
+
+{
+  name: "Sao Tome and Principe",
+  prefix: 239,
+  flag: "st" },
+
+{
+  name: "Saudi Arabia",
+  prefix: 966,
+  flag: "sa" },
+
+{
+  name: "Senegal",
+  prefix: 221,
+  flag: "sn" },
+
+{
+  name: "Serbia",
+  prefix: 381,
+  flag: "rs" },
+
+{
+  name: "Seychelles",
+  prefix: 248,
+  flag: "sc" },
+
+{
+  name: "Sierra Leone",
+  prefix: 232,
+  flag: "sl" },
+
+{
+  name: "Singapore",
+  prefix: 65,
+  flag: "sg" },
+
+{
+  name: "Slovakia",
+  prefix: 421,
+  flag: "sk" },
+
+{
+  name: "Slovenia",
+  prefix: 386,
+  flag: "si" },
+
+{
+  name: "Solomon Islands",
+  prefix: 677,
+  flag: "sb" },
+
+{
+  name: "Somalia",
+  prefix: 252,
+  flag: "so" },
+
+{
+  name: "South Africa",
+  prefix: 27,
+  flag: "za" },
+
+{
+  name: "South Korea",
+  prefix: 82,
+  flag: "kr" },
+
+{
+  name: "South Sudan",
+  prefix: 211,
+  flag: "ss" },
+
+{
+  name: "Spain",
+  prefix: 34,
+  flag: "es" },
+
+{
+  name: "Sri Lanka",
+  prefix: 94,
+  flag: "lk" },
+
+{
+  name: "Sudan",
+  prefix: 249,
+  flag: "sd" },
+
+{
+  name: "Suriname",
+  prefix: 597,
+  flag: "sr" },
+
+{
+  name: "Sweden",
+  prefix: 46,
+  flag: "se" },
+
+{
+  name: "Switzerland",
+  prefix: 41,
+  flag: "ch" },
+
+{
+  name: "Syria",
+  prefix: 963,
+  flag: "sy" },
+
+{
+  name: "Taiwan",
+  prefix: 886,
+  flag: "tw" },
+
+{
+  name: "Tajikistan",
+  prefix: 992,
+  flag: "tj" },
+
+{
+  name: "Tanzania",
+  prefix: 255,
+  flag: "tz" },
+
+{
+  name: "Thailand",
+  prefix: 66,
+  flag: "th" },
+
+{
+  name: "Timor-Leste",
+  prefix: 670,
+  flag: "tl" },
+
+{
+  name: "Togo",
+  prefix: 228,
+  flag: "tg" },
+
+{
+  name: "Tonga",
+  prefix: 676,
+  flag: "to" },
+
+{
+  name: "Trinidad and Tobago",
+  prefix: 1,
+  flag: "tt" },
+
+{
+  name: "Tunisia",
+  prefix: 216,
+  flag: "tn" },
+
+{
+  name: "Turkey",
+  prefix: 90,
+  flag: "tr" },
+
+{
+  name: "Turkmenistan",
+  prefix: 993,
+  flag: "tm" },
+
+{
+  name: "Tuvalu",
+  prefix: 688,
+  flag: "tv" },
+
+{
+  name: "Uganda",
+  prefix: 256,
+  flag: "ug" },
+
+{
+  name: "Ukraine",
+  prefix: 380,
+  flag: "ua" },
+
+{
+  name: "United Arab Emirates",
+  prefix: 971,
+  flag: "ae" },
+
+{
+  name: "United Kingdom",
+  prefix: 44,
+  flag: "gb" },
+
+{
+  name: "United States",
+  prefix: 1,
+  flag: "us" },
+
+{
+  name: "Uruguay",
+  prefix: 598,
+  flag: "uy" },
+
+{
+  name: "Uzbekistan",
+  prefix: 998,
+  flag: "uz" },
+
+{
+  name: "Vanuatu",
+  prefix: 678,
+  flag: "vu" },
+
+{
+  name: "Vatican City",
+  prefix: 379,
+  flag: "va" },
+
+{
+  name: "Venezuela",
+  prefix: 58,
+  flag: "ve" },
+
+{
+  name: "Vietnam",
+  prefix: 84,
+  flag: "vn" },
+
+{
+  name: "Yemen",
+  prefix: 967,
+  flag: "ye" },
+
+{
+  name: "Zambia",
+  prefix: 260,
+  flag: "zm" },
+
+{
+  name: "Zimbabwe",
+  prefix: 263,
+  flag: "zw" }];
