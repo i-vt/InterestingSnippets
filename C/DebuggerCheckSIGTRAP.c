94b6cf8d7096ba19801cbc37cab468e9837e863f
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-15 03:04:39 -0400

    Update DebuggerCheckSIGTRAP.c

--- C/DebuggerCheckSIGTRAP.c
+++ C/DebuggerCheckSIGTRAP.c
@@ -3,6 +3,14 @@
 #include <signal.h>
 #include <unistd.h>
 
+//Anti-Debugging
+//Before proceeding with its main activity, GorillaBot performs checks to detect the presence of debugging
+//tools. One of its first actions is to read the /proc/self/status file and inspect the TracerPid field. This
+//field indicates whether the process is being traced – a value of 0 means it’s not, while a non-zero value
+//suggests a debugger is attached.
+
+//https://any.run/cybersecurity-blog/gorillabot-malware-analysis/
+
 // Signal handler for SIGTRAP
 void handle_sigtrap(int sig) {
     printf("[!] SIGTRAP caught: No debugger detected (or it's ignoring the signal).\n");
