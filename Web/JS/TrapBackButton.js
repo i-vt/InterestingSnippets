f065994ce1381b0f8a8acc46cfc9b0113d4e0552
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-16 01:54:49 -0400

    Update TrapBackButton.js

--- Web/JS/TrapBackButton.js
+++ Web/JS/TrapBackButton.js
@@ -1,10 +1,16 @@
     // Interfere with history to trap back button
-    (function () {
-        history.replaceState(null, document.title, location.pathname + location.search);
-        for (let i = 0; i < 10; i++) {
-            try {
-                history.pushState(null, document.title, location.pathname + location.search);
-            } catch (e) {
-                break;
-            }
+    (function (window, location, document, history) {
+    history.replaceState(null, document.title, location.pathname + location.search);
+    for (let i = 0; i < 10; i++) {
+        try {
+            history.pushState(null, document.title, location.pathname + location.search);
+        } catch (e) {
+            break;
         }
+    }
+
+    window.addEventListener("popstate", function () {
+        history.replaceState(null, document.title, location.pathname + location.search);
+        window.onbeforeunload = null;
+        window.location.replace(finalRedirectUrl);
+    });
