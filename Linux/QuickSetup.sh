85b0b8d4f61615ebd416e87e95d846565b101460
i-vt <99633800+i-vt@users.noreply.github.com>
2025-04-23 21:56:32 -0400

    Update QuickSetup.sh

--- Linux/QuickSetup.sh
+++ Linux/QuickSetup.sh
@@ -16,16 +16,17 @@ sudo apt clean
 # Download and save the .vimrc file to the home directory
 wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=$HOME/.vimrc || { echo 'Failed to download .vimrc'; exit 1; }
 
-# Display the current IP address
-echo "-------[Current IP]-------"
-curl https://api.ipify.org/ || { echo 'Failed to fetch IP address'; exit 1; }
-echo ""
 
 # Shortcuts
 echo "alias s2020='python3 -m http.server 2020'" >> ~/.bashrc
 echo "alias s2021='python3 -m http.server 2021'" >> ~/.bashrc
 echo "alias s2022='python3 -m http.server 2022'" >> ~/.bashrc
 source ~/.bashrc
+
+# Display the current IP address
+echo "-------[Current IP]-------"
+curl https://api.ipify.org/ || { echo 'Failed to fetch IP address'; exit 1; }
+echo ""
 
 # Basic system info
 echo "-------[System Info]-------"
