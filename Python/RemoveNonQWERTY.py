313d8bee4a762522845e50bdef944bdda1db5ea7
i-vt <99633800+i-vt@users.noreply.github.com>
2025-05-11 00:41:59 -0400

    Update RemoveNonQWERTY.py

--- Python/RemoveNonQWERTY.py
+++ Python/RemoveNonQWERTY.py
@@ -7,6 +7,24 @@ def clean_string(input_string):
     cleaned = re.sub(allowed_chars_pattern, '', input_string)
     return cleaned
 
+# Predefined characters, currently not used
+def keep_predefined_chars(input_str):
+    predefined_keys_list = [
+        "'", ",", ".", "p", "y", "f", "g", "c", "r", "l", "/", "=", "[", "]", "\\",
+        "a", "o", "e", "u", "i", "d", "h", "t", "n", "s", "-", ";",
+        "q", "j", "k", "x", "b", "m", "w", "v", "z",
+        "\"", "<", ">", "P", "Y", "F", "G", "C", "R", "L", "?", "+", "{", "}", "|",
+        "A", "O", "E", "U", "I", "D", "H", "T", "N", "S", "_", ":",
+        "Q", "J", "K", "X", "B", "M", "W", "V", "Z", " "
+    ]
+    
+    predefined_keys = set(predefined_keys_list)
+
+    # Keep only characters in the Dvorak key set
+    filtered = ''.join(char for char in input_str if char in predefined_keys)
+    return filtered
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Clean a string or file by removing disallowed characters.')
     parser.add_argument('-t', '--text', type=str, help='Text to be cleaned')
