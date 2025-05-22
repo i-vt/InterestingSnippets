f014b237c69fe4818979f58b5499c7acfe2de73e
i-vt <99633800+i-vt@users.noreply.github.com>
2025-05-22 03:50:49 -0400

    Update QuickSetup.sh

--- Linux/QuickSetup.sh
+++ Linux/QuickSetup.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 export DEBIAN_FRONTEND=noninteractive
 
+# Check if /etc/resolv.conf already has 8.8.8.8 or 1.1.1.1
+if ! grep -qE 'nameserver (8\.8\.8\.8|1\.1\.1\.1)' /etc/resolv.conf; then
+  echo "Neither 8.8.8.8 nor 1.1.1.1 found. Adding 8.8.8.8..."
+  echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
+else
+  echo "Nameserver already set correctly."
+fi
 
 # Check if sudo is installed
 if command -v sudo >/dev/null 2>&1; then
@@ -23,13 +30,14 @@ fi
 
 
 
+
 # Update and upgrade the system packages non-interactively
 sudo apt update -y && sudo apt upgrade -y || { echo 'Failed to update and upgrade packages'; exit 1; }
 
 
 
 # Install necessary packages
-sudo apt install -y curl tree python3-pip plocate snapd python3-venv tmux git-all || { echo 'Failed to install packages'; exit 1; }
+sudo apt install -y zip curl tree python3-pip plocate snapd python3-venv tmux git-all || { echo 'Failed to install packages'; exit 1; }
 
 sudo snap refresh 
 
@@ -47,13 +55,7 @@ sudo apt clean
 # Download and save the .vimrc file to the home directory
 wget https://raw.githubusercontent.com/i-vt/InterestingSnippets/refs/heads/main/Linux/.vimrc --output-document=$HOME/.vimrc || { echo 'Failed to download .vimrc'; exit 1; }
 
-# Check if /etc/resolv.conf already has 8.8.8.8 or 1.1.1.1
-if ! grep -qE 'nameserver (8\.8\.8\.8|1\.1\.1\.1)' /etc/resolv.conf; then
-  echo "Neither 8.8.8.8 nor 1.1.1.1 found. Adding 8.8.8.8..."
-  echo "nameserver 8.8.8.8" | sudo tee -a /etc/resolv.conf > /dev/null
-else
-  echo "Nameserver already set correctly."
-fi
+
 
 # Define aliases to add
 ALIASES=(
