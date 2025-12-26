5983c7267b31921b53337b161f98180e14d08b7a
i-vt <99633800+i-vt@users.noreply.github.com>
2025-12-26 06:26:03 -0500

    Update GetAbsoluteFilepathFromDirForAllFiles.py

--- Python/GetAbsoluteFilepathFromDirForAllFiles.py
+++ Python/GetAbsoluteFilepathFromDirForAllFiles.py
@@ -1,11 +1,22 @@
 import os
 
-filepath = "./"  # or any starting directory
-all_files = []
+def get_all_file_paths(start_dir="./"):
+    """
+    Walk through a directory and return a list of absolute file paths.
+    
+    :param start_dir: Directory to start searching from
+    :return: List of absolute file paths
+    """
+    all_files = []
 
-for root, dirs, files in os.walk(filepath):
-    for file in files:
-        full_path = os.path.abspath(os.path.join(root, file))
-        all_files.append(full_path)
+    for root, dirs, files in os.walk(start_dir):
+        for file in files:
+            full_path = os.path.abspath(os.path.join(root, file))
+            all_files.append(full_path)
 
-print(all_files)
+    return all_files
+
+
+# Example usage
+files = get_all_file_paths("./")
+print(files)
