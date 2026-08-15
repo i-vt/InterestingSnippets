2eeefa5b6baa33f13cc481e35dd88b078829db3d
i-vt <99633800+i-vt@users.noreply.github.com>
2026-08-14 22:18:03 -0400

    Change output of sayНello function
    Updated the malicious function to print a different message in Cyrillic.

---
+++ Python/HomoglyphFunctionConfusionPoC.py
@@ -0,0 +1,7 @@
+def sayHello():          # Latin 'H' (U+0048)
+    print("Hello, World!")
+
+def sayНello():          # Cyrillic 'Н' (U+041D)
+    print("Хело!")
+
+sayНello()               # Calls the Cyrillic version
